73a1b0fccee7d0c7d5d10dd28f09c830052a6f0e
Штирлиц <152861800+Dfhjtssg@users.noreply.github.com>
2025-05-22 07:32:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const map = L.map('map', {
   zoomControl: false
-}).setView([42.856174, 74.497395], 14); // пример
+}).setView([42.836928, 74.603946], 14); // пример
 
 L.control.zoom({
   position: 'topright'
